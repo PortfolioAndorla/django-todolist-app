7b8c59533406dd58da046184e7cd35545eecd98a
Andorla0 <84116120+Andorla0@users.noreply.github.com>
2025-07-08 07:09:38 -0500

    feat(tasks): implement CRUD operations and completed tasks view

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Task
+from .forms import TaskForm
+from django.db.models import Case, When, IntegerField, Value
 
-# Create your views here.
+
+def task_list(request):
+    tasks = Task.objects.annotate(
+        priority_order=Case(
+            When(priority='high', then=Value(1)),
+            When(priority='medium', then=Value(2)),
+            When(priority='low', then=Value(3)),
+            output_field=IntegerField()
+        )).order_by('priority_order', 'due_date')
+
+    return render(request, 'tasks/task_list.html', {'tasks': tasks})
+
+
+def task_create(request):
+    if request.method == 'POST':
+        form = TaskForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('task_list')
+    else:
+        form = TaskForm()
+
+    return render(request, 'tasks/task_form.html', {'form': form})
+
+
+def task_edit(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    form = TaskForm(request.POST or None, instance=task)
+    if form.is_valid():
+        form.save()
+        return redirect('task_list')
+    return render(request, 'tasks/task_form.html', {'form': form})
+
+
+def task_delete(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.delete()
+    return redirect('task_list')
+
+def task_complete(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = True
+    task.save()
+    return redirect('task_list')
+
+
+def completed_tasks_list(request):
+    task = Task.objects.filter(is_completed=True)
+    return render(request, 'tasks/completed_tasks_list.html', {'tasks': task})
